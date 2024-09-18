1a45f00e790963b867c78d581f8d481b6509b2c3
Máté Gábor Seidl <mate.seidl@tum.de>
2024-09-18 22:23:52 +0200

    folders and avalonia app created

---
+++ 04_Click_Detection_App/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace _04_Click_Detection_App;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
