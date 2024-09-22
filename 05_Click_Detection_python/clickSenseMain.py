ace37c74c403be04ceff51ce7eabd2efca8234ee
Máté Gábor Seidl <mate.seidl@tum.de>
2024-09-22 18:51:17 +0200

    clickSense visualization update

--- 05_Click_Detection_python/clickSenseMain.py
+++ 05_Click_Detection_python/clickSenseMain.py
@@ -4,37 +4,42 @@ import signal
 import sys
 import threading
 import time
-from visulizeAudioInput import AudioPlotter 
+from visulizeAudioInputAmplitude import AudioAmplitudePlotter 
 
 # Constants
-sampling_rate = 48000
+sampling_rate_orig = 48000
 channels = 1
-chunk = 4096
 format = pyaudio.paInt16
 
 class ClickSense:
     def __init__(self):
         self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(format=format, channels=channels, rate=sampling_rate, input=True, frames_per_buffer=chunk)
+        self.chunk = 1024
+        self.sampling_rate_downsampled = int(sampling_rate_orig/3) # 16000
+        self.stream = self.p.open(format=format, channels=channels, rate=self.sampling_rate_downsampled, input=True, frames_per_buffer=self.chunk)
         self.recording = False
         self.amplitude = 0.0
         self.lock = threading.Lock()
+        self.audio_data = None
 
     def start_recording(self):
         self.recording = True
         
         while self.recording:
-            data = self.stream.read(chunk)
+            data = self.stream.read(self.chunk)
             audio_data = np.frombuffer(data, dtype=np.int16)
-            amplitude = np.linalg.norm(audio_data) / len(audio_data)
+            #amplitude = np.linalg.norm(audio_data) / len(audio_data)
+            amplitude = audio_data
             
             with self.lock:
                 self.amplitude = amplitude
+                self.audio_data = audio_data
             
-            time.sleep(0.05)  # 50 ms
+            #time.sleep(0.0001)  
 
     def get_amplitude(self):
         with self.lock:
+            #return self.audio_data
             return self.amplitude
 
     def stop_recording(self):
@@ -56,6 +61,6 @@ if __name__ == '__main__':
     recording_thread = threading.Thread(target=click_sense.start_recording)
     recording_thread.start()
 
-    audio_plotter = AudioPlotter(click_sense)
+    audio_plotter = AudioAmplitudePlotter(click_sense)
 
     recording_thread.join()
--- 05_Click_Detection_python/visulizeAudioInputAmplitude.py
+++ 05_Click_Detection_python/visulizeAudioInputAmplitude.py
@@ -2,31 +2,31 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
-x_lim = 100
-y_lim = 300
+# full range of int16 (https://learn.microsoft.com/en-us/dotnet/api/system.int16?view=net-8.0)
+y_lim = 32768
 
-class AudioPlotter:
+class AudioAmplitudePlotter:
     def __init__(self, click_sense):
         self.click_sense = click_sense
+        x_lim = (self.click_sense.chunk)*4
         self.fig, self.ax = plt.subplots()
+        self.fig.canvas.manager.set_window_title('clickSense - Amplitude Plotter')
         self.line, = self.ax.plot(np.zeros(x_lim))
-        self.ax.set_ylim(0, y_lim)  # Adjust based on the expected range of amplitude
+        self.ax.set_ylim(-y_lim, y_lim)
         self.ax.set_xlim(0, x_lim)
-
         self.fig.canvas.mpl_connect('close_event', self.handle_close)
-
-        self.ani = animation.FuncAnimation(self.fig, self.update, interval=50, blit=True)
+        self.ani = animation.FuncAnimation(self.fig, self.update, interval=0.064)
         plt.show()
 
     def update(self, frame):
         amplitude = self.click_sense.get_amplitude()
         if amplitude is None:
-            amplitude = 0.0 
+            amplitude = np.zeros(self.click_sense.chunk) 
 
-        # Update the line data
         data = self.line.get_ydata()
-        data = np.roll(data, -1)
-        data[-1] = amplitude
+
+        data = np.roll(data, -self.click_sense.chunk)
+        data[-self.click_sense.chunk:] = amplitude
         self.line.set_ydata(data)
         return self.line,
 
