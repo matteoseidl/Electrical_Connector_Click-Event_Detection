f771e56438e69416dc59d9b6117f95e656dce42d
Máté Gábor Seidl <mate.seidl@tum.de>
2024-10-03 12:16:34 +0200

    training and model saving

---
+++ 03_Click_Detection_Model/01_modelArchitectures/ClickDetectorCNN_v1.py
@@ -0,0 +1,38 @@
+# CNN model for click detection
+
+import torch
+from torch import nn
+
+class ClickDetectorCNN(nn.Module):
+    def __init__(self, input_channels, output_shape):
+        super().__init__()
+        self.block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_channels, 
+                      out_channels=64, 
+                      kernel_size=3,
+                      stride=1,
+                      padding="same"),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, 
+                      out_channels=64, 
+                      kernel_size=3,
+                      stride=1,
+                      padding="same"),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=32*64*8,
+                      out_features=output_shape),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, x: torch.Tensor):
+        x = self.block_1(x)
+        x = self.block_2(x)
+        x = self.classifier(x)
+        return x
