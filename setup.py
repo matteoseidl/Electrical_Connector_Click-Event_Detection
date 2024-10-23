123046daa3563e603ee29c243026fa0b2b453b17
Máté Gábor Seidl <mate.seidl@tum.de>
2024-10-23 17:57:20 +0200

    folder structure update

--- setup.py
+++ setup.py
@@ -4,10 +4,20 @@ import sys
 
 # missing dictionaries to create
 directories = [
-    '01_Dataset/01_ethernet_without_additional_noise',
-    '01_Dataset/01_audioDatasets/02_ethernet_with_additional_noise',
+    '01_Dataset/01_audioDatasets',
+    '01_Dataset/01_audioDatasets/01_Ethernet',
+    '01_Dataset/01_audioDatasets/02_Ethernet_Test',
+    '01_Dataset/01_audioDatasets/03_HVA280',
+    '01_Dataset/01_audioDatasets/04_HVA280_Test',
+    '01_Dataset/01_audioDatasets/05_HVA630',
+    '01_Dataset/01_audioDatasets/06_HVA630_Test',
+    '01_Dataset/01_audioDatasets/07_Noise_Samples',
 
-    '02_Data_Augmentation/01_augmented_datasets',
+    '02_Data_Augmentation/01_augmentedDatasets',
+    '02_Data_Augmentation/01_augmentedDatasets/01_Ethernet',
+    '02_Data_Augmentation/01_augmentedDatasets/02_HVA280',
+    '02_Data_Augmentation/01_augmentedDatasets/03_HVA630',
+    '02_Data_Augmentation/01_augmentedDatasets/04_Noise_Samples',
 
     '03_Click_Detection_Model/02_savedWeights',
 ]
